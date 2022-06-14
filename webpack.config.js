8cccc155577075f3269fadbd11a29de04adb3d02
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-06-14 10:55:56 +0200

    feat: add ts support & npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,25 @@ const dist_folder = 'dist';
 export default {
     mode: 'development',
     entry: {
-        bundle: path.resolve(__dirname, 'src/index.js')
+        bundle: './src/index.ts'
     },
     output: {
         path: path.resolve(__dirname, dist_folder),
         filename: '[name].js',
         clean: true,
         assetModuleFilename: '[name][ext]'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
     },
     devtool: 'source-map'
 }
