684d9b610527c54f2e2fc43d46cf55db8b270bdd
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-06-18 20:12:51 +0200

    feat: add basic cluster functionality

---
+++ src/multi.ts
@@ -0,0 +1,27 @@
+import cluster from "cluster";
+import { cpus } from "os";
+
+import runServer from './server.js';
+
+const cpuNumber = cpus().length;
+
+
+const runCluster = (number: number = cpuNumber) => {
+    if (cluster.isPrimary) {
+        cluster.schedulingPolicy = cluster.SCHED_RR;
+
+        for (let i = 0; i < number; i++) {
+            cluster.fork()
+        }
+
+        cluster.on('exit', (worker) => {
+            console.log(`Worker ${worker.process.pid} has exited`);
+        });
+    } else {
+        runServer(true);
+
+        console.log(`Worker process ${process.pid} started`);
+    }
+}
+
+runCluster();
